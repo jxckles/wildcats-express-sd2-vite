c0fff25ad7701a29b85c40cf60d24a7c2cc333af
jxckles <dojolesjaier9@gmail.com>
2025-01-24 03:07:43 +0800

    updated home ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Auth from "./components/auth/home.jsx";
+import Home from "./components/auth/home.jsx";
 import AdminPage from "./components/admin-page/admin-page.jsx";
 import PosPage from "./components/pos-client-page/pos-page.jsx";
 import Login from "./components/auth/login-page/login";
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Auth />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login-page" element={<Login />} />
           <Route path="/admin-page" element={<AdminPage />} />
           <Route path="/pos-page" element={<PosPage />} />
