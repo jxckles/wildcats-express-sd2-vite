c7e8d83627c3fd72f5aa7b49b3ee139e140f829e
darylcitu <daryl.butar@cit.edu>
2025-01-31 22:50:45 +0800

    Minor Improvements: Redirection, Logout function
    1. Logout function - moved to firebaseConfig for modularity. Para no redundancy in case we have other pages that needs this function.
    
    2. Issue when logged out: When trying to access Admin page (or any other page) while not signed in, the page is still rendered before redirecting back to login. [FIXED: Now shows white screen]
    
    3. Issue when logged in: When trying to access Login page while already signed in, the page is still rendered before redirecting back. [FIXED: Now shows white screen]
    
    Suggestion (Optional):
    - Instead of white screen, show loading animation or just a picture of our mascot (if kaya hehe)

--- src/components/admin-page/admin-page.jsx
+++ src/components/admin-page/admin-page.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { auth, redirectToLoginIfLoggedOut } from "../../config/firebase-config";
+import { redirectToLoginIfLoggedOut, handleLogout } from "../../config/firebase-config";
 import { motion, AnimatePresence } from "framer-motion";
 import { FaSignOutAlt, FaChartBar, FaClipboardList, FaCog } from "react-icons/fa";
 import {LuTrello, LuHandPlatter, LuPlus} from "react-icons/lu";
@@ -13,6 +12,9 @@ import "react-toastify/dist/ReactToastify.css";
 import "./admin-page.css";
 
 const AdminPage = () => {
+  const navigate = useNavigate();
+  const loading = redirectToLoginIfLoggedOut(navigate);
+
   const [activeTab, setActiveTab] = useState("dashboard");
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [menuItems, setMenuItems] = useState([]);
@@ -24,20 +26,10 @@ const AdminPage = () => {
     quantity: 0,
   });
 
-  const navigate = useNavigate();
+  if (loading) {
+    return <div>Loading...</div>; // Prevent UI from showing unless logged in
+  }
 
-  redirectToLoginIfLoggedOut(navigate);
-
-  const signUserOut = async () => {
-    try {
-      await signOut(auth);
-      localStorage.clear();
-      navigate("/");
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  
   const openModal = () => {
     setNewMenuItem({
       name: "",
@@ -239,181 +231,183 @@ const renderMenuItems = () => (
 
   // Render admin Page
   return (
-    <motion.div 
-      className="admin-page"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
-    >
-      <div className="header">
-        <div className="logo-container">
-          <img 
-            src={catLogo} 
-            alt="Wildcats Express Logo" 
-            className="logo"       
-          />
-          <h1>Wildcats Express</h1>
+    <>
+      <motion.div 
+        className="admin-page"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className="header">
+          <div className="logo-container">
+            <img 
+              src={catLogo} 
+              alt="Wildcats Express Logo" 
+              className="logo"       
+            />
+            <h1>Wildcats Express</h1>
+          </div>
+
+          <div className="profile-section" onClick={toggleDropdown}>
+            <img
+              src={catProfile}
+              alt="Admin"
+              className="profile-photo"
+            />
+            <span className="profile-name">Admin</span>
+
+            <AnimatePresence mode="sync">
+              {isDropdownOpen && (
+                <motion.div
+                  className="dropdown-menu"
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -20 }}
+                  transition={{ duration: 0.2 }}
+                >
+                  <div className="dropdown-item" onClick={() => handleTabChange("adminReports")}>
+                    <FaClipboardList /> Admin Reports
+                  </div>
+                  <div className="dropdown-item" onClick={() => handleTabChange("analytics")}>
+                    <FaChartBar /> Analytics
+                  </div>
+                  <div className="dropdown-item" onClick={() => handleTabChange("settings")}>
+                    <FaCog /> Settings
+                  </div>
+                  <div className="dropdown-item"  onClick={() => handleLogout(navigate)}>
+                    <FaSignOutAlt /> Sign Out
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
         </div>
 
-        <div className="profile-section" onClick={toggleDropdown}>
-          <img
-            src={catProfile}
-            alt="Admin"
-            className="profile-photo"
-          />
-          <span className="profile-name">Admin</span>
+        <div className="sidebar">
+          <nav>
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              onClick={() => handleTabChange("dashboard")}
+              className={activeTab === "dashboard" ? "active" : ""}
+            >
+              <LuTrello className="dashboard-icon"/>
+            </motion.button>
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              onClick={() => handleTabChange("addMenu")}
+              className={activeTab === "addMenu" ? "active" : ""}
+            >
+              <LuPlus className="addmenu-icon" />
+            </motion.button>
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              onClick={() => handleTabChange("orders")}
+              className={activeTab === "orders" ? "active" : ""}
+            >
+              <LuHandPlatter className="orders-icon" />
+            </motion.button>
+          </nav>
+        </div>
 
-          <AnimatePresence>
-            {isDropdownOpen && (
-              <motion.div
-                className="dropdown-menu"
-                initial={{ opacity: 0, y: -20 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -20 }}
-                transition={{ duration: 0.2 }}
-              >
-                <div className="dropdown-item" onClick={() => handleTabChange("adminReports")}>
-                  <FaClipboardList /> Admin Reports
-                </div>
-                <div className="dropdown-item" onClick={() => handleTabChange("analytics")}>
-                  <FaChartBar /> Analytics
-                </div>
-                <div className="dropdown-item" onClick={() => handleTabChange("settings")}>
-                  <FaCog /> Settings
-                </div>
-                <div className="dropdown-item" onClick={signUserOut}>
-                  <FaSignOutAlt /> Sign Out
-                </div>
-              </motion.div>
-            )}
+        <div className="main-content-admin">
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={activeTab}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.3 }}
+            >
+              {activeTab === "dashboard" && (
+                <motion.div 
+                  className="dashboard"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.2 }}
+                >
+                  <h2>Welcome, Admin!</h2>
+                  <br />
+                  <p>Every customer counts. Let's serve them better today!</p>
+                </motion.div>
+              )}
+              {activeTab === "addMenu" && (
+                <motion.div 
+                  className="add-menu"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.2 }}
+                >
+                  <h3>Welcome, Admin!</h3>
+                  <br/>
+                  <h2>Every customer counts. Let's serve them better today!</h2>
+                  <br/>
+                  <div>{renderAddmenu()}</div>
+                  <br/>
+                  <div>{renderMenuItems()}</div>
+                </motion.div>
+              )}
+              {activeTab === "orders" && (
+                <motion.div 
+                  className="orders"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.2 }}
+                >
+                  <h2>Orders</h2>
+                </motion.div>
+              )}
+              {activeTab === "adminReports" && (
+                <motion.div 
+                  className="admin-reports"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.2 }}
+                >
+                  <h2>Admin Reports</h2>
+                </motion.div>
+              )}
+              {activeTab === "analytics" && (
+                <motion.div 
+                  className="analytics"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.2 }}
+                >
+                  <h2>Analytics</h2>
+                </motion.div>
+              )}
+              {activeTab === "settings" && (
+                <motion.div 
+                  className="settings"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.2 }}
+                >
+                  <h2>Settings</h2>
+                </motion.div>
+              )}
+            </motion.div>
           </AnimatePresence>
+          
         </div>
-      </div>
-
-      <div className="sidebar">
-        <nav>
-          <motion.button
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            onClick={() => handleTabChange("dashboard")}
-            className={activeTab === "dashboard" ? "active" : ""}
-          >
-            <LuTrello className="dashboard-icon"/>
-          </motion.button>
-          <motion.button
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            onClick={() => handleTabChange("addMenu")}
-            className={activeTab === "addMenu" ? "active" : ""}
-          >
-            <LuPlus className="addmenu-icon" />
-          </motion.button>
-          <motion.button
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            onClick={() => handleTabChange("orders")}
-            className={activeTab === "orders" ? "active" : ""}
-          >
-            <LuHandPlatter className="orders-icon" />
-          </motion.button>
-        </nav>
-      </div>
-
-      <div className="main-content-admin">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={activeTab}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{ duration: 0.3 }}
-          >
-            {activeTab === "dashboard" && (
-              <motion.div 
-                className="dashboard"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2 }}
-              >
-                <h2>Welcome, Admin!</h2>
-                <br />
-                <p>Every customer counts. Let's serve them better today!</p>
-              </motion.div>
-            )}
-            {activeTab === "addMenu" && (
-              <motion.div 
-                className="add-menu"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2 }}
-              >
-                <h3>Welcome, Admin!</h3>
-                <br/>
-                <h2>Every customer counts. Let's serve them better today!</h2>
-                <br/>
-                <div>{renderAddmenu()}</div>
-                <br/>
-                <div>{renderMenuItems()}</div>
-              </motion.div>
-            )}
-            {activeTab === "orders" && (
-              <motion.div 
-                className="orders"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2 }}
-              >
-                <h2>Orders</h2>
-              </motion.div>
-            )}
-            {activeTab === "adminReports" && (
-              <motion.div 
-                className="admin-reports"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2 }}
-              >
-                <h2>Admin Reports</h2>
-              </motion.div>
-            )}
-            {activeTab === "analytics" && (
-              <motion.div 
-                className="analytics"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2 }}
-              >
-                <h2>Analytics</h2>
-              </motion.div>
-            )}
-            {activeTab === "settings" && (
-              <motion.div 
-                className="settings"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.2 }}
-              >
-                <h2>Settings</h2>
-              </motion.div>
-            )}
-          </motion.div>
-
-          <ToastContainer 
-            position="top-center"
-            autoClose={5500}
-            hideProgressBar
-            newestOnTop
-            closeButton={false}
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            style={{ zIndex: 9999 }}  
-          />
-        </AnimatePresence>
-      </div>
-    </motion.div>
+      </motion.div>
+      <ToastContainer 
+              position="top-center"
+              autoClose={5500}
+              hideProgressBar
+              newestOnTop
+              closeButton={false}
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              style={{ zIndex: 9999 }}  
+      />
+    </>
   );
 };
 
