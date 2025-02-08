2a6fb2f58658aab175d77d42a7837d6383e7f143
jxckles <dojolesjaier9@gmail.com>
2025-02-08 16:55:17 +0800

    Update admin-page.jsx

--- src/components/admin-page/admin-page.jsx
+++ src/components/admin-page/admin-page.jsx
@@ -721,12 +721,17 @@ const AdminPage = () => {
       onChange={(e) => setSelectedYear(e.target.value)}
     >
       <option value="">Year</option>
-      <option value="2024">2024</option>
       <option value="2025">2025</option>
+      <option value="2026">2026</option>
       <option value="2026">2027</option>
       <option value="2028">2028</option>
       <option value="2029">2029</option>
       <option value="2030">2030</option>
+      <option value="2031">2031</option>
+      <option value="2032">2032</option>
+      <option value="2033">2033</option>
+      <option value="2034">2034</option>
+      <option value="2035">2035</option>
       {/* Add other years */}
     </select>
 
