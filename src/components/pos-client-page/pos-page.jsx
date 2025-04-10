9eb50f6d409dda67ed34321e9363f4f53fd62101
K4nnaKamui <149076405+K4nnaKamui@users.noreply.github.com>
2025-04-10 21:48:07 +0800

    text hot and cold edit

--- src/components/pos-client-page/pos-page.jsx
+++ src/components/pos-client-page/pos-page.jsx
@@ -323,6 +323,7 @@ const handleOrderNumberChange = (e) => {
         </motion.div>
           <motion.div className="category-filter" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.3 }}>
             {["All", "Rice", "Dishes", "Coffee", "Drinks", "Snacks"].map((cat) => (
+            {["All", "Rice", "Dishes", "Hot Drinks", "Cold Drinks", "Snacks"].map((cat) => (
               <motion.button key={cat} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className={selectedCategory === cat ? "active-category" : ""} onClick={() => setSelectedCategory(cat)}>
                 {cat}
               </motion.button>
